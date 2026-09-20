dd3b097d6241d7c4e00e7ca03e79cef9f67b5e66
Jøran T <jor.tve782@gokstadakademiet.no>
2026-09-20 17:32:32 +0200

    Added finished subtask 4.3 to task 4

--- oppgave_4_program.py
+++ oppgave_4_program.py
@@ -1,3 +1,5 @@
+# Oppgave 4.3
+
 from oppgave_4_helpers import *
 
 report_path = file_dir / "support-rapport.txt"
@@ -9,7 +11,8 @@ amount_tickets = valid_ticket_count(valid_tickets)
 categories = ticket_per_category(valid_tickets)
 total, avg = ticket_time_spent(valid_tickets)
 solved, unsolved = tickets_solved_unsolved(valid_tickets)
-highest_category = most_tickets(categories)
+top_category, top_count = most_tickets(categories)
+unsolved_sorted = unsolved_tickets_sorted(valid_tickets)
 
 
 
@@ -17,19 +20,27 @@ with open(report_path, "w", encoding="utf-8") as file:
     file.write("= RAPPORT FOR SUPPORTHENVENDELSER =\n")
     file.write("-----------------------------------\n")
     file.write("\n== Antall gyldige henvendelser og antall i hver kategori ==\n")
-    file.write(f"\nDet er {amount_tickets} gyldige henvendelser og antall per kategori fordeler seg slik: {categories}\n")
+    file.write(f"\nDet er totalt {amount_tickets} gyldige henvendelser.\n")
+    file.write("\nFordeling per kategori: \n")
+    for c, n in categories.items():
+        file.write(f"  * {c}: {n} henvendelser\n")
     file.write("\n-------------------------------------\n")
     file.write("\n== Samlet og gjennomsnittlig tidsbruk ==\n")
     file.write(f"\nSamlet tidsbruk for henvendelsene er: {total} minutter.\n")
-    file.write(f"Gjennomsnittlig tidsbruk for henvendelsene er: {avg} minutter.\n")
+    file.write(f"Gjennomsnittlig tidsbruk per henvendelsene er: {avg} minutter.\n")
     file.write("\n----------------------------------\n")
     file.write("\n == Antall løste og uløste henvendelser ==\n")
     file.write(f"\n Antall løste henvendelser: {solved}")
     file.write(f"\n Antall uløste henvendelser: {unsolved}")
-    file.write(f"test {highest_category}")
-    file.write("")
-    file.write("")
-
+    file.write(f"\n---------------------------------\n")
+    file.write("\n== Kategorien med flest henvendelser ==\n")
+    file.write(f"\n Kategori '{top_category}' har {top_count} henvendelser og er dermed kategorien med flest henvendelser.\n")
+    file.write("\n------------------------------------\n")
+    file.write("\n == Uløste henvendelser per mest tidkrevende == \n")
+    file.write("\nFordeling per id og tidsbruk: \n")
+    for m, i in unsolved_sorted:
+        file.write(f"  * Henvendelse med ID nummer {i}: Antall minutter brukt: {m} \n")
+    file.write("\n------------------------------------\n")
 
 
 
